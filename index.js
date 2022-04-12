899c74ee2b5ba7dab2db32b5d6b5320ff245f860
GabrielBessi <tecbessi@gmail.com>
2022-04-12 15:47:55 -0300

    Conclusão do exercício S3-10, aonde tinhamos 3 competidores sendo que um deles tinha uma vantagem por ter concluído algumas metas durante a prova e o objetivo era colocá-los em ordem de chegada correta

---
+++ index.js
@@ -0,0 +1,15 @@
+let placement = [
+    "firstPlace",
+    "secondPlace",
+    "lastPlace",
+]
+
+function positions(firstPlace, secondPlace, lastPlace){
+    
+    console.log(placement[0] + " " + firstPlace)
+    console.log(placement[1] + " " + secondPlace)
+    console.log(placement[2] + " " + lastPlace)
+    
+}
+
+positions(" Daniel", "Rafael", "Manoel" )
